5f454d1b2137c9a05cecb5947805f1ad4f75f298
Rolf <vyprolf@gmail.com>
2017-02-07 15:18:44 +0100

    send update dialog

--- public/javascripts/timingsvue.js
+++ public/javascripts/timingsvue.js
@@ -1,8 +1,13 @@
 function initVue() {
+    let eventBus = new Vue();
+
     Vue.component('horse-race', {
         props: ['servername', 'data', 'now'],
-        template: '<div class="timingRow"> <h4>{{servername}}</h4> <div><horse-tier v-for="n in 8" :tier="n" :currentTier="data.horseClass"></horse-tier><time-status :now="now" :start="data.startTime" :registered="data.registeredTime"></time-status></div> </div>',
+        template: '<div class="timingRow"> <h4>{{servername}}</h4> <div><horse-tier v-for="n in 8" :tier="n" :currentTier="data.horseClass"></horse-tier><time-status :now="now" :start="data.startTime" :registered="data.registeredTime"></time-status><button style="position:absolute; right: 3px; top:3px; border-radius: 15px;" class="btn btn-xs btn-info glyphicon glyphicon-cloud-upload" @click="updateTiming"></button></div></div>',
         methods: {
+            updateTiming: function(){
+                eventBus.$emit('updateTiming', this.servername);
+            },
             currentStatus: function (diffInMinutes) {
                 if (diffInMinutes > 1)
                     return "Last observed race was";
@@ -24,12 +29,15 @@ function initVue() {
             }
         },
         methods: {
-            parseTime: function(time){
-                if( time < -60 ) return `Last observed race was over ${Math.floor(Math.abs(time / 60))} hours ago`;
-                if( time < -5 ) return `Last observed race was ${time} minutes ago`;
-                if( time > -5 && time < 0 ) return "registration active";
+            minutesToHours: function(minutes){
+                return Math.floor(Math.abs( minutes / 60 ));
+            },
+            parseTime: function(minutes){
+                if( minutes < -60 ) return `Last observed race was over ${this.minutesToHours(minutes)} hours ago`;
+                if( minutes < -5 ) return `Last observed race was ${minutes} minutes ago`;
+                if( minutes > -5 && minutes < 0 ) return "registration active";
 
-                return `next race in ${time} minutes`;
+                return `next race in ${minutes} minutes`;
             }
         },
         computed: {
@@ -59,11 +67,12 @@ function initVue() {
                 return {
                     'btn': true,
                     'btn-primary': !condition,
-                    'btn-success': condition
+                    'btn-success': condition,
+                    'horse-tier': true
                 }
             }
         },
-        template: '<span v-bind:class="cssClass" style="padding: 5px 10px">{{tier}}</span>'
+        template: '<span v-bind:class="cssClass">{{tier}}</span>'
     });
 
     Vue.component('region-select', {
@@ -89,6 +98,54 @@ function initVue() {
         template: '<button v-bind:class="cssClass" style="font-size: 125%" @click="changeRegion">{{this.name()}}</button>'
     });
 
+    Vue.component('timing-dialog', {
+        data: function(){
+            return {
+                enabled: false,
+                server: {},
+                tier: 1,
+                time: 0
+            }
+        },
+        created: function(){
+            eventBus.$on('update-dialog', data => {
+                this.server = data;
+                this.enabled = true;
+            });
+        },
+        methods: {
+            onClose: function(){
+                console.log("closing");
+                this.enabled = false;
+            },
+            onSend: function(){
+                const payload = {
+                    region: this.server.region,
+                    serverid: this.server.id,
+                    horseClass: this.tier,
+                    //TODO: Only add the extra 5 minutes if a "registration is available" checkbox is unchecked
+                    time: new Date().getTime() + seconds( Number(this.time) * 60) + 300000,
+                    // user: elementValue('update-user'),
+                    // password: elementValue('update-password'),
+                };
+
+                fetch("/update", {
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                    },
+                    method: "POST",
+                    body: JSON.stringify(payload)
+                }).then( () => {
+                    eventBus.$emit('refresh-timings');
+                    this.time = 0;
+                    this.onClose();
+                }).catch( () => alert("failed to send") );
+            }
+        },
+        template: '<div v-if="enabled" class="outer-dialog"><div class="inner-dialog"><h4>Update race timing for {{server.servername}} on {{server.region}}<strong></strong> </h4><input type="text" v-model="time" placeholder="number in minutes"><br/> <span style="padding-right: 5px;" v-for="n in 8"><input v-model="tier" type="radio" v-bind:value="n">{{n}}</input></span><br/><button @click="onSend">Send</button><button @click="onClose">Close</button></div></div>'
+    });
+
     let timingapp = new Vue({
         el: '#bd-app',
         data: {
@@ -107,6 +164,18 @@ function initVue() {
                     hash = 'eu';
                 this.setRegion(hash);
                 setInterval( () => this.now = new Date().getTime(), 1000 );
+                eventBus.$on('updateTiming', abc => {
+                    console.log("updateTiming");
+                    eventBus.$emit('update-dialog', {
+                        region: this.region,
+                        id: this.servernames.indexOf(abc),
+                        servername: abc
+                    });
+                });
+
+                eventBus.$on('refresh-timings', () =>{
+                    this.refreshData();
+                });
             },
             fetchServerNames: function () {
                 fetch('/servernames')
