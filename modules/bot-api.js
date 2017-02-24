7bc6e09d2591d5438f3075a2cc906eb06929dd47
Rolf <vyprolf@gmail.com>
2017-02-24 10:07:20 +0100

    refactored parameter parsing to bot-api, removed role filter

---
+++ modules/bot-api.js
@@ -0,0 +1,105 @@
+const db = require('./db');
+
+let BotApi = {};
+
+const authorizedChannels = [
+    "bot-test"
+    // "eu_seasoned_race_times",
+    // "us_seasoned_race_times"
+];
+
+const vroomUrl = "https://rolfdev.net";
+
+const serverNames = ["velia 2","balenos 2","serendia 2","calpheon 2","mediah 2","valencia 2"];
+
+const helpTexts = [
+    "!race :region :server :tier :time :registration",
+    "!race [us|eu] [Velia|Balenos|Serendia|Calpheon|Mediah|Valencia] [Tier?|1|2|3|4|5|6|7|8] [50m] [optional:registration]",
+    "[Notes: I am very sensitive to case, so e.g. 'Tier' is not the same as 'tier']"
+];
+
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+/**
+ * @return {boolean}
+ */
+BotApi.IsValidChannel = function(channel){
+    return authorizedChannels.indexOf(channel) === -1;
+};
+
+BotApi.HelpTexts = function(){
+    return helpTexts;
+};
+
+/**
+ * @return {string}
+ */
+BotApi.GetVroomUrl = function(){
+    return vroomUrl;
+};
+
+/**
+ * @return {string}
+ */
+BotApi.ParseRace = function(botCommand){
+    const splits = botCommand.toLowerCase().split(' ');
+
+    if( splits.length !== 5 && splits.length !== 6 ){
+        return "Invalid Parameter Count: " + splits.length;
+    }
+
+    const region = splits[1];
+    if( region !== 'us' && region !== 'eu' ){
+        return `Invalid Parameter for 'region': '${region}'`;
+    }
+
+    const server = splits[2];
+    const serverIndex = serverNames.indexOf( server + " 2");
+
+    if( serverIndex === -1 ){
+        return `Invalid Parameter for 'server': ${server}`;
+    }
+
+    const tier = splits[3];
+    const tierId = tier.replace("tier", '');
+    if ( tier.startsWith("Tier") && (Number(tierId) < 1 || Number(tierId) > 8) && tierId !== '?' ) {
+        return `Invalid Parameter for 'tier': ${tier}[${tierId}]`
+    }
+
+    const minuteParameter = splits[4];
+    let minutes = minuteParameter.replace('m', '');
+    if (Number(minutes) < 0 || Number(minutes) > 60  ) {
+        return "Invalid Parameter for 'minutes': " + minutes;
+    }
+
+
+    let registrationActive = false;
+    if( splits[5] !== undefined && splits[5] === "registration" ){
+        if( Number(minutes) < 0 || Number(minutes) > 5 ){
+            return "Invalid value for 'minutes' when parameter 'registration' is specified: " + minutes;
+        }
+
+        registrationActive = true;
+        minutes -= 5;
+    }
+
+    let timestamp = new Date().getTime() + Number(minutes) * 60 * 1000;
+    if( this.registrationAvailable )
+        timestamp -=  (5 * 60 + 15) * 60 * 1000;
+
+
+    db.update(region, serverIndex, timestamp, tierId);
+
+
+    let minuteString = registrationActive ?
+                        "registration closes in " + minutes :
+                        "${minutes}m untill registration";
+
+    return `User ${message.author.username} just updated ${capitalizeFirstLetter(serverNames[serverIndex])}[${region}] with Tier ${tierId} & ${minuteString}`;
+};
+
+
+module.exports = BotApi;
--- modules/discordbot.js
+++ modules/discordbot.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const db = require('./db');
+const botApi = require('./bot-api');
 
 const discordClient = new Discord.Client();
 
@@ -13,13 +13,11 @@ discordClient.on('message', message => {
     if( message.author.bot === true )
         return;
 
-    if( message.channel.name !== "bot-test" )
+    if( !botApi.IsValidChannel(message.channel.name) )
         return;
 
-    /** Authorize **/
-     // channel == seasoned_eu_race && role == eu_race_timing
-    //or
-    // channel == seasoned_us_race && role == us_race_timing
+    /** TODO: Authorize Roles **/
+
 
     /** Parse Command **/
 
@@ -27,80 +25,18 @@ discordClient.on('message', message => {
      //foreach token
 
     if( message.content === "!cmd" || message.content === "!command" || message.content === "!race" || message.content === "!help" ){
-        message.channel.sendMessage("!race :region :server :tier :time :registration");
-        message.channel.sendMessage("!race [us|eu] [Velia|Balenos|Serendia|Calpheon|Mediah|Valencia] [Tier?|1|2|3|4|5|6|7|8] [50m] [optional:registration]");
-        message.channel.sendMessage("[Notes: I am very sensitive to case, so e.g. 'Tier' is not the same as 'tier']");
+        botApi.HelpTexts().forEach(t => message.channel.sendMessage(t) );
         return;
     }
 
     else if( message.content === "!link" ){
-        message.channel.sendMessage("https://rolfdev.net");
+        message.channel.sendMessage(botApi.GetVroomUrl());
         return;
     }
 
     else if( message.content.startsWith('!race ') ){
-        const moderatorRole = message.member.roles.has("283937603345186816");
-
-        if( moderatorRole === false ) {
-            message.channel.sendMessage("Apologies, but only moderators may register races");
-            return;
-        }
-
-        const splits = message.content.split(' ');
-
-        if( splits.length !== 5 && splits.length !== 6 ){
-            message.channel.sendMessage("Invalid Parameter Count: " + splits.length);
-            return;
-        }
-        const serverNames = ["Velia 2","Balenos 2","Serendia 2","Calpheon 2","Mediah 2","Valencia 2"];
-
-        const region = splits[1];
-
-        if( region !== 'us' && region !== 'eu' ){
-            message.channel.sendMessage("Invalid Parameter for 'region': " + region);
-            return;
-        }
-
-        const server = splits[2];
-        const serverIndex = serverNames.indexOf( server + " 2");
-
-        if( serverIndex === -1 ){
-            message.channel.sendMessage("Invalid Parameter for 'server': " + server);
-            return;
-        }
-
-        const tier = splits[3];
-        const tierId = tier.replace("Tier", '');
-        if ( tier.startsWith("Tier") && (Number(tierId) < 1 || Number(tierId) > 8) && tierId !== '?' ) {
-            message.channel.sendMessage(`Invalid Parameter for 'tier': ${tier}[${tierId}]`);
-            return;
-        }
-
-        const minuteParameter = splits[4];
-        let minutes = minuteParameter.replace('m', '');
-        if (Number(minutes) < 0 || Number(minutes) > 60  ) {
-            message.channel.sendMessage("Invalid Parameter for 'minutes': " + minutes);
-            return;
-        }
-
-
-        if( splits[5] !== undefined && splits[5] === "registration" ){
-            if( Number(minutes) < 0 || Number(minutes) > 5 ){
-                message.channel.sendMessage("Invalid value for 'minutes' when parameter 'registration' is specified: " + minutes);
-                return;
-            }
-
-            minutes -= 5;
-        }
-
-        let timestamp = new Date().getTime() + Number(minutes) * 60 * 1000;
-        if( this.registrationAvailable )
-            timestamp -=  (5 * 60 + 15) * 60 * 1000;
-
-
-        db.update(region, serverIndex, timestamp, tierId);
-
-        message.channel.sendMessage(`User ${message.author.username} just updated ${serverNames[serverIndex]}[${region}] with Tier ${tierId} & ${minutes}m left` )
+        const response = botApi.ParseRace(message.content);
+        message.channel.sendMessage(response);
     }
 });
 
